a449adceb021fa3a2a41de4fd86ac9c8843162b3
Abdelmounaim Bakkari <bakkari.abdelmounaim@gmail.com>
2022-01-21 15:01:33 +0100

    major commit

---
+++ routes/route.js
@@ -0,0 +1,21 @@
+ const express = require('express');
+ const router = express.Router();
+ const{allUsers, userForm, saveUser, editUser, updateUser, deleteUser} = require('../controllers/user_ctrl');
+ const{allDepartements,  departementForm, saveDepartement} = require('../controllers/departement_ctrl');
+
+ ///ROUTER USER///
+
+ router.get('/', allUsers);
+ router.get('/views/create', userForm);
+ router.get('/edit/:id', editUser);
+ router.post('/update', updateUser);
+ router.get('/delete/:id', deleteUser);
+router.post('/saveUser', saveUser)
+
+///ROUTER DEPARTEMENT///
+router.get('/departements', allDepartements);
+router.get('/views/creer', departementForm);
+router.post('/saveDepartement', saveDepartement)
+
+
+ module.exports = router;
